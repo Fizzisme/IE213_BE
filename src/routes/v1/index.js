1df2f84103b6ebf34713634190175ab37279d19c
Phuc Phan <phandinphhuc090870@gmail.com>
2026-03-10 20:30:29 +0700

    Mount thêm route admin vào index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { userRoute } from '~/routes/v1/user.route';
 import { authRoute } from '~/routes/v1/auth.route';
-
+import { adminUserRoute } from '~/routes/v1/adminUser.route';
 const Router = express.Router();
 
 // Auth API
 Router.use('/auth', authRoute);
 // User API
 Router.use('/user', userRoute);
+
+// Admin API  ← THÊM
+Router.use('/admin', adminUserRoute);
 export const APIs_V1 = Router;
