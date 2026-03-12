a99ea9e7334b7d235161dcd7c3022e28a28aaf6d
Phuc Phan <phandinphhuc090870@gmail.com>
2026-03-12 10:25:54 +0700

    feat(admin-user): Thêm hàm login trong adminAuth.controller.js

--- src/controllers/adminAuth.controller.js
+++ src/controllers/adminAuth.controller.js
@@ -1,11 +1,33 @@
-import bcrypt from 'bcrypt';
+import ms from 'ms';
 import { StatusCodes } from 'http-status-codes';
-import { env } from '~/config/environment';
-import { userModel } from '~/models/user.model';
-import { adminModel } from '~/models/admin.model';
-import { JwtProvider } from '~/providers/JwtProvider';
-import ApiError from '~/utils/ApiError';
+import { adminAuthService } from '~/services/adminAuth.service';
+
+// Lưu ý: Admin ko login bằng wallet chỉ login bằng căn cước
+const login = async (req, res, next) => {
+    try {
+        const result = await adminAuthService.login(req.body);
+
+        res.cookie('accessToken', result.accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: ms('20 minutes'),
+        });
+
+        res.cookie('refreshToken', result.refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: ms('14 days'),
+        });
+
+        res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
 
 export const adminAuthService = {
-    
+    login,
 };
