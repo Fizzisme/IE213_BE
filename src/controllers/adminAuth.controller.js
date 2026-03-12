9b16ce5c967879c35bc4ddb38c457828778db218
Phuc Phan <phandinphhuc090870@gmail.com>
2026-03-12 10:13:16 +0700

    feat(admin-user): Tạo adminAuth.controller.js trong ccontrollers

---
+++ src/controllers/adminAuth.controller.js
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt';
+import { StatusCodes } from 'http-status-codes';
+import { env } from '~/config/environment';
+import { userModel } from '~/models/user.model';
+import { adminModel } from '~/models/admin.model';
+import { JwtProvider } from '~/providers/JwtProvider';
+import ApiError from '~/utils/ApiError';
+
+export const adminAuthService = {
+    
+};
